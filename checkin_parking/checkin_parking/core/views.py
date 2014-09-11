465869f52ecc11c7b3052d0f79f762a15c834a79
Alexander Kavanaugh <akavanau@calpoly.edu>
2014-09-11 16:15:01 -0700

    Added core package

--- checkin_parking/checkin_parking/core/views.py
+++ checkin_parking/checkin_parking/core/views.py
@@ -7,10 +7,10 @@
 """
 
 
-from django.http import HttpResponseNotFound, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.urlresolvers import reverse_lazy
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.cache import never_cache
@@ -18,16 +18,18 @@ from django.views.decorators.csrf import csrf_protect
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
+
+from rmsconnector.forms import RMSAuthenticationForm
+from rmsconnector.utils import Resident
 
 
 class IndexView(TemplateView):
     template_name = "core/index.html"
 
     def get_context_data(self, **kwargs):
-
         context = super(TemplateView, self).get_context_data(**kwargs)
+
+        # TODO: Add Session data to context
 
         return context
 
@@ -45,6 +47,7 @@ class LoginView(FormView):
 
     template_name = 'core/login.html'
     form_class = AuthenticationForm
+    rms_form_class = RMSAuthenticationForm
 
     @method_decorator(sensitive_post_parameters())
     @method_decorator(csrf_protect)
@@ -52,8 +55,26 @@ class LoginView(FormView):
     def dispatch(self, *args, **kwargs):
         return super(LoginView, self).dispatch(*args, **kwargs)
 
-    def form_valid(self, form):
+    def get(self, request, *args, **kwargs):
+        form = self.get_form(self.form_class)
+        rms_form = self.get_form(self.rms_form_class)
 
+        return self.render_to_response(self.get_context_data(form=form, rms_form=rms_form))
+
+    def post(self, *args, **kwargs):
+        form = self.get_form(self.form_class)
+        rms_form = self.get_form(self.rms_form_class)
+
+        if self.request.POST["user_type"] == "resident":
+            if rms_form.is_valid():
+                return self.form_valid(rms_form)
+        else:
+            if form.is_valid():
+                return self.form_valid(form)
+
+        return self.forms_invalid(form, rms_form)
+
+    def form_valid(self, form):
         # Authenticate the user against LDAP
         username = form.cleaned_data['username']
         password = form.cleaned_data['password']
@@ -62,18 +83,32 @@ class LoginView(FormView):
         if user.is_authenticated():
             auth_login(self.request, user)
 
-        # Check if user is new tech
-        if self.request.user.is_technician:
-            if self.request.user.is_new_tech == None:  # First time log in, set flag from None to True
-                self.request.user.is_new_tech = True
-                self.request.user.save()
-
-        if self.request.user.is_new_tech:
-            self.success_url = reverse_lazy('orientation_checklist')
-        else:
-            self.success_url = self.request.GET.get("next", reverse_lazy('home'))
+        self.success_url = self.request.GET.get("next", reverse_lazy('home'))
 
         return super(LoginView, self).form_valid(form)
+
+    def rms_form_valid(self, form):
+        # Authenticate the user against RMS
+        alias = form.cleaned_data['alias']
+        dob = form.cleaned_data['dob']
+        user = authenticate(alias=alias, dob=dob)
+
+        if user.is_authenticated():
+            resident = Resident(alias=alias)
+
+            try:
+                resident.get_address()
+            except ObjectDoesNotExist:
+                raise ValidationError("The alias provided does not match University Housing records for the current term.")
+            else:
+                auth_login(self.request, user)
+
+        self.success_url = self.request.GET.get("next", reverse_lazy('home'))
+
+        return super(LoginView, self).form_valid(form)
+
+    def forms_invalid(self, form, rms_form):
+        return self.render_to_response(self.get_context_data(form=form, rms_form=rms_form))
 
 
 def logout(request):
