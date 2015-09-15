e24e56c62962e8dcc2d4459f6e16cfec972900e6
willson556 <thomas.willson@me.com>
2015-09-14 19:17:31 -0700

    Hotfix 4838: If user leaves a reserve page open they could previously still submit an ajax request to reserve another slot later.

--- checkin_parking/apps/reservations/ajax.py
+++ checkin_parking/apps/reservations/ajax.py
@@ -7,6 +7,7 @@
 """
 from _datetime import datetime
 
+from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.views.decorators.http import require_POST
 from django_ajax.decorators import ajax
@@ -20,6 +21,12 @@ from .tasks import send_confirmation_email
 def reserve_slot(request):
     slot_id = request.POST['slot_id']
     change_reservation = request.POST.get('change_reservation', False)
+
+    try:
+        ReservationSlot.objects.get(resident=request.user)
+        raise ValidationError('You have already reserved a slot. Please refresh this page.')
+    except ReservationSlot.DoesNotExist:
+        pass
 
     query = ReservationSlot.objects.filter(timeslot__id=slot_id, zone__buildings__name__contains=request.user.building, resident=None)
 
