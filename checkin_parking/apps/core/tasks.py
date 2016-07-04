02fdf88cfeb639c335fd6f1268c7a08bd90639ba
Thomas Willson <tewillso@calpoly.edu>
2016-07-04 16:43:41 -0700

    Forgot to include file in previous commit.

---
+++ checkin_parking/apps/core/tasks.py
@@ -0,0 +1,28 @@
+"""
+.. module:: checkin_parking.apps.core.tasks
+   :synopsis: Checkin Parking Core Tasks
+
+.. moduleauthor:: Thomas Willson <thomas.willson@icloud.com>
+
+"""
+from concurrent.futures import ThreadPoolExecutor
+
+from rmsconnector.utils import Resident
+from uwsgidecorators import cron
+
+from ..administration.models import AdminSettings
+from ..zones.models import Building
+from .models import CheckinParkingUser
+
+
+@cron(0, 2, -1, -1, -1)
+def update_users_buildings():
+    def update_user_building(user):
+        address_dict = Resident(principal_name=user.email, term_code=AdminSettings.objects.get_settings().term_code)
+
+        if address_dict['building']:
+            user.building = Building.objects.get(name=address_dict['building'], community__name=address_dict['community'])
+            user.save()
+
+    with ThreadPoolExecutor(max_workers=20) as pool:
+        pool.map(update_user_building, CheckinParkingUser.objects.all())
