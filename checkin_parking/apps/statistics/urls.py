7c943cf87f950f83f1cdc93c784f400dccac99b4
Thomas Willson <tewillso@calpoly.edu>
2016-07-17 14:09:45 -0700

    Add preliminary statistics functionality by generating a CSV of reservations for download. (CPRK-7)

--- checkin_parking/apps/statistics/urls.py
+++ checkin_parking/apps/statistics/urls.py
@@ -11,10 +11,12 @@ from django.contrib.auth.decorators import login_required
 
 from ...urls import administrative_access
 from ..core.views import IndexView
+from .views import CSVStatisticsView
 
 
 app_name = 'statistics'
 
 urlpatterns = [
     url(r'^$', login_required(administrative_access(IndexView.as_view())), name='statistics'),  # Not implemented yet. See (CPRK-7)
+    url(r'^csv', login_required(administrative_access(CSVStatisticsView.as_view())), name='csv'),
 ]
