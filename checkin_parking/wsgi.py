2ab98129079b86e9c149dc09a3859f6b1c646cd0
Thomas Willson <tewillso@calpoly.edu>
2016-07-19 19:23:18 -0700

    Another attempted fix for bad gateway error relating to Oracle DB.

--- checkin_parking/wsgi.py
+++ checkin_parking/wsgi.py
@@ -3,15 +3,13 @@
 from .manage import activate_env
 
 activate_env()
-
 import django  # noqa
-from django.core.handlers.wsgi import WSGIHandler  # noqa
+from django.core.wsgi import get_wsgi_application  # noqa
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry  # noqa
 
-django.setup()
 
 # Import any functions with uWSGI decorators here:
 from .apps.reservations import tasks  # noqa
 from .apps.core import tasks  # noqa
 
-application = Sentry(WSGIHandler())
+application = Sentry(get_wsgi_application())
