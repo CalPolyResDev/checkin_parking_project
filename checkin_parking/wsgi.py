c8470c457efbba77331e29dcf5c4df4b156e7d21
Chase Voorhees <cvoorhee@calpoly.edu>
2016-09-15 15:22:24 -0700

    Revert "Another attempted fix for bad gateway error relating to Oracle DB."
    This reverts commit 2ab98129079b86e9c149dc09a3859f6b1c646cd0.

--- checkin_parking/wsgi.py
+++ checkin_parking/wsgi.py
@@ -3,13 +3,15 @@
 from .manage import activate_env
 
 activate_env()
+
 import django  # noqa
-from django.core.wsgi import get_wsgi_application  # noqa
+from django.core.handlers.wsgi import WSGIHandler  # noqa
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry  # noqa
 
+django.setup()
 
 # Import any functions with uWSGI decorators here:
 from .apps.reservations import tasks  # noqa
 from .apps.core import tasks  # noqa
 
-application = Sentry(get_wsgi_application())
+application = Sentry(WSGIHandler())
