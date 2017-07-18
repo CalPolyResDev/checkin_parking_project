daf5654822f718774e7e45f4d0fc4068540376ed
fedorareis <fedorareis@gmail.com>
2017-07-18 00:13:35 -0700

    Updated index table

--- checkin_parking/apps/core/views.py
+++ checkin_parking/apps/core/views.py
@@ -49,6 +49,7 @@ class IndexView(TemplateView):
                 "time_range": first_reservation.timeslot.time.strftime(settings.PYTHON_TIME_FORMAT) + " - " + delta.strftime(settings.PYTHON_TIME_FORMAT),
                 "community": first_reservation.zone.community.name,
                 "out_of_state": first_reservation.out_of_state,
+                "assisted_move_in": first_reservation.assisted_move_in
                 "buildings": ', '.join(first_reservation.zone.buildings.values_list('name', flat=True)),
             })
 
