e066e403d8d03873bb2bae0eedf7d52e082450bc
fedorareis <fedorareis@gmail.com>
2017-08-04 04:57:53 -0700

    email?

--- checkin_parking/apps/administration/utils.py
+++ checkin_parking/apps/administration/utils.py
@@ -70,11 +70,11 @@ def sync_user_data():
         # Write data to DB
         admin_settings = AdminSettings.objects.get_settings()
         for resident in residents:
-            CheckinParkingUser.objects.create(username=resident.email, 
+            CheckinParkingUser.objects.create(username=resident.principal_name, 
                                               first_name=resident.first_name, 
                                               last_name=resident.last_name, 
                                               full_name=resident.first_name + " " + resident.last_name, 
-                                              email=resident.email,
+                                              email=resident.principal_name,
                                               building=Building.objects.get(name=resident.address_dict['building'].replace('_', ' '), community__name=resident.address_dict['community']) if resident.address_dict['building'] else None,
                                               term_type=resident.application_term_type(application_term=admin_settings.application_term, application_year=admin_settings.application_year),
                                               out_of_state=None,)
