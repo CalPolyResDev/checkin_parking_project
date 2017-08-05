4d8f1c319f7bfcc014019121ab0996fb60eb5dfe
fedorareis <fedorareis@gmail.com>
2017-08-05 14:47:48 -0700

    Testing a thing

--- checkin_parking/apps/core/backends.py
+++ checkin_parking/apps/core/backends.py
@@ -95,8 +95,7 @@ class CASLDAPBackend(CASBackend):
                     user.building = Building.objects.get(name=resident.address_dict['building'].replace('_', ' '), community__name=resident.address_dict['community']) if resident.address_dict['building'] else None
                     user.term_type = resident.application_term_type(application_term=admin_settings.application_term, application_year=admin_settings.application_year)
 
-                print("Username: " + user.username)
-                print("Principal Name: " + str(user_info["userPrincipalName"]))
+                logger.warning("Username: " + user.username + "\nPrincipal Name: " + str(user_info["userPrincipalName"]))
 
                 user.full_name = user_info["displayName"]
                 user.first_name = user_info["givenName"]
